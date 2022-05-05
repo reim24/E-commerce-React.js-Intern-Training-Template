698eae557e089b5f33009b0bdce55948817da5f0
Grigori Godole <grigorgodole@gmail.com>
2022-05-05 10:46:07 +0200

    login form changed

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -5,13 +5,27 @@ import onLogin from "../../main/store/stores/user/login.store.on-login"
 import './test.css'
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const TestPage: FC = () => {
 
-
+    const theme = createTheme()
     const [userName, setUserName] = useState(null)
     const [password, setPassword] = useState(null)
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const notify = () => toast("Welcome")
@@ -22,40 +36,83 @@ const TestPage: FC = () => {
     }
 
 
+
     const handleButtonClick = () => {
         navigate("/Register", { replace: true });
     }
 
     return (
-        <>
-            <section className="wrapper">
-                <div className="login">
-                    <h1>Login</h1>
-                    <div className="container">
-                        <label className="labelBlock">Username</label>
-                        <input
-                            className='input'
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box >
+                        <TextField
                             onChange={(e) => setUserName(e.target.value)}
-                        ></input>
-
-                        <label className="labelBlock" >password</label>
-                        <input type='password' className='input'
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="User Name"
+                            name="userName"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
                             onChange={(e) => setPassword(e.target.value)}
-                        ></input>
-                    </div>
-                    <div className="button_container">
-                        <button className="button__login" onClick={() => {
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button onClick={() => {
                             handleSubmit()
-                            notify()
-                        }
-                        }>Submit</button>
-                        <button className="button__login" onClick={() => handleButtonClick()}>Register Now</button>
-                    </div>
-                </div>
-            </section>
-
-        </>
+                            if (handleSubmit) {
+                                notify()
+                            }
+                        }}
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item>
+                                <Link href="#" variant="body2" onClick={() => { handleButtonClick() }}>
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     )
+
+
 }
 
 export default TestPage
