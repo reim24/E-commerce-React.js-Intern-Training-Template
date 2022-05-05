1541fa5b12473c5f52c2f2fff5a2767ff31cbd5d
Grigori Godole <grigorgodole@gmail.com>
2022-05-05 10:53:03 +0200

    registerDone

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -2,9 +2,23 @@ import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import onRegister from "../../main/store/stores/user/register.store.on-register"
 import './register.css'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Register = () => {
 
+    const theme = createTheme()
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
@@ -34,46 +48,122 @@ const Register = () => {
         navigate('/', { replace: true })
     }
     return (
-        <section className="wrapper">
-            <div className="register">
-                <h1 >Register</h1>
-                <form className="container" onSubmit={handleSubmit} >
 
-                    <label className="labelBlock">
-                        First Name:
-                        <input className='inputRegister' type="text" name="firstName" />
-                    </label>
-                    <label className="labelBlock">
-                        Last Name:
-                        <input className='inputRegister' type="text" name="lastName" />
-                    </label>
-                    <label className="labelBlock">
-                        Email:
-                        <input className='inputRegister' type="email" name="email" />
-                    </label>
-                    <label className="labelBlock">
-                        Birthday:
-                        <input className='inputRegister' type="number" name="birthdate" />
-                    </label>
-                    <label className="labelBlock">
-                        Phone:
-                        <input className='inputRegister' type="text" name="phone" />
-                    </label>
-                    <label className="labelBlock">
-                        Username:
-                        <input className='inputRegister' type="text" name="username" />
-                    </label>
-                    <label className="labelBlock">
-                        Password:
-                        <input className='inputRegister' type="password" name="password" />
-                    </label>
-                    <label className="labelBlock_register">
-                        <input className='button__register' type="submit" value="Register" />
-                    </label>
-                </form>
-                <p onClick={handleClick} className="info">Go to login page</p>
-            </div>
-        </section>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="birthdate"
+                                    label="birthdate"
+                                    name="birthdate"
+                                    autoComplete="birthdate"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="phone"
+                                    label="phone"
+                                    name="phone"
+                                    autoComplete="phone"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="username"
+                                    name="username"
+                                    autoComplete="username"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+                            </Grid>
+
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item onClick={handleClick}>
+                                <Link href="#" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     )
 }
 export default Register
